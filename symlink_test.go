1d7b1a83acf1e1c776fb4ae5157b756b3ef8b1af
VonC <vonc@laposte.net>
2015-10-18 05:29:16 +0200

    symlink_test.go: use array of test

--- symlink_test.go
+++ symlink_test.go
@@ -13,7 +13,14 @@ import (
 //   - is a symlink pointing to the same
 //   - is a folder x, rename it to x.1 (if x.1 exists, x.2, ...)
 
+type test struct {
+	dst string
+	err string
+	sl  *SL
+}
+
 func TestDestination(t *testing.T) {
+
 	// only a nil bit will make filepath.Abs() fail:
 	// https://github.com/golang/go/blob/d16c7f8004bd1c9f896367af7ea86f5530596b39/src/syscall/syscall_windows.go#L41
 	// from UTF16FromString (https://github.com/golang/go/blob/d16c7f8004bd1c9f896367af7ea86f5530596b39/src/syscall/syscall_windows.go#L71)
@@ -21,18 +28,20 @@ func TestDestination(t *testing.T) {
 	// from abs (https://github.com/golang/go/blob/d16c7f8004bd1c9f896367af7ea86f5530596b39/src/path/filepath/path_windows.go#L109)
 	// from Abs (https://github.com/golang/go/blob/d16c7f8004bd1c9f896367af7ea86f5530596b39/src/path/filepath/path.go#L235)
 
+	tests := []*test{
+		&test{dst: "unknown/dst", err: "The system cannot find the path specified"},
+		&test{dst: string([]byte{0}), err: "invalid argument"},
+	}
 	var sl *SL
 	var err error
-	sl, err = New("unknown/dst")
-	if err == nil || strings.Contains(err.Error(), "The system cannot find the path specified") == false {
-		t.Errorf("Err '%v', expected 'The system cannot find the path specified'", err)
-	}
-	if sl != nil {
-		t.Errorf("SL '%v', expected <nil>", sl)
-	}
-	_, err = New(string([]byte{0}))
-	if err == nil || strings.Contains(err.Error(), "invalid argument") == false {
-		t.Errorf("Err '%v', expected 'invalid argument'", err)
+	for _, test := range tests {
+		sl, err = New(test.dst)
+		if err == nil || strings.Contains(err.Error(), test.err) == false {
+			t.Errorf("Err '%v', expected '%s'", err, test.err)
+		}
+		if sl != nil {
+			t.Errorf("SL '%v', expected <nil>", sl)
+		}
 	}
 	New(".")
 }
