86db698ac0baa34ac5fdcc085a6ec93bf6ed0f8a
VonC <vonc@laposte.net>
2015-12-01 16:08:46 +0100

    symlink_test.go: test symlink reference to a file.

--- symlink_test.go
+++ symlink_test.go
@@ -1,6 +1,8 @@
 package gosymlink
 
 import (
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 )
@@ -18,9 +20,20 @@ var testsDir = []testDir{
 	testDir{dirpath: "nonexistentdir/"},
 	// Dir cannot reference a file
 	testDir{dirpath: "afile.txt", errormsg: "is not a folder, it is a file"},
+	// Dir cannot reference a file through a junction link
+	testDir{dirpath: "ltofile", errormsg: "is not a folder, it is a file"},
+}
+
+func initTestDir(t *testing.T) {
+	if _, err := os.Stat("tests/ltofile"); err != nil {
+		if _, err = execcmd("mklink", fmt.Sprintf("/J %s %s", "ltofile", "afile.txt"), "tests"); err != nil {
+			t.Errorf("Impossible to create junction between '%s' and '%s':\n'%+v'", "ltofile", "afile.txt", err)
+		}
+	}
 }
 
 func TestDir(t *testing.T) {
+	initTestDir(t)
 	for _, tst := range testsDir {
 		path := "tests/" + tst.dirpath
 		dir, err := DirFrom(path)
